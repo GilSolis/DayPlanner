0b151227d90888141b270bb1902021c871959978
Gil Solis <gilsolisuw@gmail.com>
2020-01-13 21:30:43 -0800

    counter now stops when questions end, also added time remaining bonus to final score

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var time = moment()
+var time = moment();
 
-console.log(time);
-$("parent").append(time._d);
+console.log(time.utc);
+$("h1").append(` ${time._d}`);
+$("h1").fadeOut(2000);
 console.log(time._d);
-// $("parent").text(time._d);
-document.getElementById("parent").innerHTML = time._d
+// $("h1").text(`${time._d}`);
+// $("<p>").text(`${time._d}`)
+// document.getElementById("parent").innerHTML = time._d
